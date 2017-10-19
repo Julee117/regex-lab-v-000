be69ae0d7d723a6999ada35435a0f7f7e1296031
Julianne Kim <juliannne@gmail.com>
2017-10-19 22:00:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,8 +19,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{10}/)|(/.(\d{3}).(\d{3})-(\d{4})/)|(/(\d{3})\s(\d{3})\s(\d{4})/)|(/.(\d{3}).(\d{7})/) ? true : false
-  # phone.match(/\d{10}/) || phone.match(/.(\d{3}).(\d{3})-(\d{4})/) ||
+  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+  # return false unless phone.match(/\d{10}/) || phone.match(/.(\d{3}).(\d{3})-(\d{4})/) ||
   #     phone.match(/(\d{3})\s(\d{3})\s(\d{4})/) || phone.match(/.(\d{3}).(\d{7})/)
   # return true
 end
